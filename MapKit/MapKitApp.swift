0207d58cdc2ba1c92746c09c98afc4a49e55373b
Dearimranullah <107716481+Dearimranullah@users.noreply.github.com>
2023-03-20 10:21:49 +0500

    Initial Commit

---
+++ MapKit/MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitApp.swift
+//  MapKit
+//
+//  Created by Apple1 on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
